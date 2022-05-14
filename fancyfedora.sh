5456e46ccb29d8d90dca06f25bb1dd6ab7ad80f8
Bubsy <95828808+BubsyOrange@users.noreply.github.com>
2022-05-14 11:26:23 -0500

    Update fancyfedora.sh

--- fancyfedora.sh
+++ fancyfedora.sh
@@ -6,22 +6,13 @@
 #
 # Standard Disclaimer: Author assumes no liability for any damage
 
-#Welcome Message
-echo " Bubsy Orange's 
-███████╗░█████╗░███╗░░██╗░█████╗░██╗░░░██╗  ███████╗███████╗██████╗░░█████╗░██████╗░░█████╗░
-██╔════╝██╔══██╗████╗░██║██╔══██╗╚██╗░██╔╝  ██╔════╝██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗
-█████╗░░███████║██╔██╗██║██║░░╚═╝░╚████╔╝░  █████╗░░█████╗░░██║░░██║██║░░██║██████╔╝███████║
-██╔══╝░░██╔══██║██║╚████║██║░░██╗░░╚██╔╝░░  ██╔══╝░░██╔══╝░░██║░░██║██║░░██║██╔══██╗██╔══██║
-██║░░░░░██║░░██║██║░╚███║╚█████╔╝░░░██║░░░  ██║░░░░░███████╗██████╔╝╚█████╔╝██║░░██║██║░░██║
-╚═╝░░░░░╚═╝░░╚═╝╚═╝░░╚══╝░╚════╝░░░░╚═╝░░░  ╚═╝░░░░░╚══════╝╚═════╝░░╚════╝░╚═╝░░╚═╝╚═╝░░╚═╝
-                                                                                    ver.0.1"
-
-
 # Update System
 sudo dnf update -y
 
 # Install RPM Fusion Free & Non-Free
 sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+
+sudo dnf update -y
 
 # Appstream metadata for RPM Fusion
 sudo dnf groupupdate core -y
@@ -32,14 +23,5 @@ flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flat
 # Install Media Codecs
 sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -y
 
-#Final Message
-echo "
-███████╗░█████╗░███╗░░██╗░█████╗░██╗░░░██╗  ███████╗███████╗██████╗░░█████╗░██████╗░░█████╗░
-██╔════╝██╔══██╗████╗░██║██╔══██╗╚██╗░██╔╝  ██╔════╝██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗
-█████╗░░███████║██╔██╗██║██║░░╚═╝░╚████╔╝░  █████╗░░█████╗░░██║░░██║██║░░██║██████╔╝███████║
-██╔══╝░░██╔══██║██║╚████║██║░░██╗░░╚██╔╝░░  ██╔══╝░░██╔══╝░░██║░░██║██║░░██║██╔══██╗██╔══██║
-██║░░░░░██║░░██║██║░╚███║╚█████╔╝░░░██║░░░  ██║░░░░░███████╗██████╔╝╚█████╔╝██║░░██║██║░░██║
-╚═╝░░░░░╚═╝░░╚═╝╚═╝░░╚══╝░╚════╝░░░░╚═╝░░░  ╚═╝░░░░░╚══════╝╚═════╝░░╚════╝░╚═╝░░╚═╝╚═╝░░╚═╝
-Enjoy your new system!"
 
 
